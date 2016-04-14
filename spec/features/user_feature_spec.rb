18ef80b60c4b4c4914434a5d5e23a6ba9cccf0f7
Caspar Fischer-Zernin <casparfz@gmail.com>
2016-04-14 09:12:13 +0100

    added user via devise, added signin/signout/signup links to layout, added welcome page controller

--- spec/features/user_feature_spec.rb
+++ spec/features/user_feature_spec.rb
@@ -13,4 +13,27 @@ feature 'Sign up and Sign in' do
       expect(page).not_to have_link('Sign out')
     end
   end
+
+  context 'User on page is signed in' do
+
+    before do
+      visit '/'
+      click_link('Sign up')
+      fill_in('Email', with: 'test@example.com')
+      fill_in('Password', with: 'testtest')
+      fill_in('Password confirmation', with: 'testtest')
+      click_button('Sign up')
+    end
+
+    it 'should have a sign out button on the website' do
+      visit '/'
+      expect(page).to have_link('Sign out')
+    end
+
+    it 'should not have a sign in/sign up button' do
+      visit '/'
+      expect(page).not_to have_link('Sign in')
+      expect(page).not_to have_link('Sign up')
+    end
+  end
 end
